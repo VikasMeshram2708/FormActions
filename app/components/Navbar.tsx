96092465a5d7d27fbc7b7f4d36eefa85cec472f9
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-07 19:39:10 +0530

    :sparkles: code updated.

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-accent text-primary-content">
+      <button className="btn btn-ghost text-xl">daisyUI</button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
+  const handleSearch = async (data: FormData) => {
+    const todo = data.get("name");
+    console.log(todo);
+  };
+
   return (
-    <main className="">
+    <main className="mt-24">
+      <form
+        action={handleSearch}
+        className="bg-accent max-w-xl mx-auto rounded p-5"
+      >
+        <div className="">
+          <input
+            type="text"
+            className="form-control p-2 rounded"
+            placeholder="Enter name"
+            name="name"
+            id="name"
+          />
+          <button type="submit" className="mt-5 btn btn-primary btn-sm">
+            Search
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
